b011e25e8b04e62c06e0b448ea631c3aedd3c72b
Administrator <Administrator@CanadaSam.ca>
2020-01-16 16:34:15 -0500

    update

--- Start-WSBExchangeBackup.ps1
+++ Start-WSBExchangeBackup.ps1
@@ -8,23 +8,30 @@
 [CmdletBinding()]
 Param(
        [String]$Server = $env:computerName,
-       [Parameter(Mandatory)][String]$BackupShare = "\\YourServer\Backups\$($env:computername)",
-       [string[]]$Databases = @("DB01","DB02","DB03"),
+       [Parameter()][String]$BackupShare = "C:\ExchangeVolumes\ExVol3",
+       [string[]]$Databases = @("DAG1-DB1","DAG1-DB2"),
        [switch]$ActiveDBsOnly
 )
  
-$BackupShare += "\"            #Adding trailing backslash
+$BackupShare += "\"            # Adding trailing backslash
+write-Verbose "Backup share:                                          $BackupShare"
  
 $RequireLocal = $ActiveDBsOnly # If =ActiveDBsOnly switch is specified, the script will skip any DBs that aren't active locally
- 
+Write-verbose "Require DBs to be Active Locally:                      $ActiveDBsOnly"
+
 # HKEY_LOCAL_MACHINE\Software\Microsoft\ExchangeServer\v14\Replay\Parameters\EnableVSSWriter = 0
 # If the Exchange server host any passive copies this key must be set to 0 or Windows Backup will fail
 # Setting this to 0 disables hardware based VSS snapshots, which will break most storage based snapshot solutions
 
 $ResetRegKey = $True # If set to $True the script will set the value back to 1 after the Windows Backup is done
-$RegParameters = "HKLM:\Software\Microsoft\ExchangeServer\v15.1\Replay\Parameters"
- 
+Write-Verbose "Reset EnableVSSWriter key to ""1"" at end of script:   $ResetRegKey"
+
+$RegParameters = "HKLM:\Software\Microsoft\ExchangeServer\v15\Replay\Parameters"
+Write-Verbose "Registry key queried:                                  $RegParameters"
+
+Write-Verbose "Enabling VSSWriter"
 $EnableVSSWriter = (Get-ItemProperty $RegParameters).EnableVSSWriter
+Write-verbose $EnableVSSWriter
  
 $Date = Get-Date
  
@@ -36,11 +43,14 @@ If ($EnableVSSWriter -ne 0) {
 }
 
 ForEach ($Database in $Databases) {
+       Write-verbose "Current database: $Database"
        $CurrentDB = Get-MailboxDatabase $Database
        $DBDrive = $CurrentDB.EdbFilePath.DriveName
+       Write-Verbose "Current drive for database $Database : $DBDrive"
        $CurrentServer = $CurrentDB.Server.Name          # Sam-Note: "Server" parameter is the same as the "MountedOnServer"  parameter of the current database.
+       Write-Verbose "Server active for DB $Database is $CurrentServer"
        If ($CurrentServer -ne $Server -and $RequireLocal) {
-              Write-Hose "-ActiveDBsOnly parameter specified => the database $Database is not active on the current server"
+              Write-Host "-ActiveDBsOnly parameter specified => the database $Database is not active on the current server"
               Write-Host "`nBackup aborted, $Database is active on $CurrentServer, which is not the same as the current server: $Server." -ForegroundColor Yellow
        }
        Else {
@@ -82,3 +92,4 @@ If ($ResetRegKey) {
         Write-Host "Restarting the Exchange Replication Service"
         Restart-Service "MSExchangeRepl"
 }
+
